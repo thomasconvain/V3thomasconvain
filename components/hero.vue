be8682b72025575145cfd78f4a13a18389e74d2b
thomas.convain <thomas.convain@gmail.com>
2024-01-10 19:54:11 -0300

    corrige z-index de form de contacto

--- components/hero.vue
+++ components/hero.vue
@@ -7,7 +7,7 @@
           <div class="relative h-full mx-auto max-w-7xl lg:px-8">
             <div class="h-full lg:grid lg:grid-cols-2 lg:gap-8 align-middle flex items-center">
               <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 text-center lg:px-0 lg:text-left flex items-center">
-                <div class="lg:py-24 z-40 relative">
+                <div class="lg:py-24 z-20 relative">
                   <a href="#" class="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200">
                     <span class="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-indigo-500 rounded-full">¿Tomemos un café?</span>
                     <button @click="openModal()" class="ml-4 text-sm">Contáctame</button>
